e8c412387964776196276cfa4318595106e396e6
Onepiece Id <fernandomontgomery@my.smccd.edu>
2019-08-18 13:59:10 +0700

    Static Files

---
+++ js/menu.js
@@ -0,0 +1,3 @@
+'use strict';(function iifeMenu(document,window,undefined){var menuBtn=document.querySelector('.menu__btn');var menu=document.querySelector('.menu__list');function toggleMenu(){menu.classList.toggle('menu__list--active');menu.classList.toggle('menu__list--transition');this.classList.toggle('menu__btn--active');this.setAttribute('aria-expanded',this.getAttribute('aria-expanded')==='true'?'false':'true')}
+function removeMenuTransition(){this.classList.remove('menu__list--transition')}
+if(menuBtn&&menu){menuBtn.addEventListener('click',toggleMenu,!1);menu.addEventListener('transitionend',removeMenuTransition,!1)}}(document,window))
